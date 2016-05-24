8ecab5d1e3b6cd7c196bc6b3d5a1b2547eda81b7
Manuel Maier <mjmaier@gmx.de>
2016-05-24 02:17:58 +0200

    Eliminate FixedBlock.inl

--- Code/Backbone/FixedBlock.hpp
+++ Code/Backbone/FixedBlock.hpp
@@ -16,41 +16,67 @@ struct fixed_block
   template<typename t_index>
   auto
   operator[](t_index Index)
-    -> decltype(Data[Index]);
+    -> decltype(Data[Index])
+  {
+    BoundsCheck(Index >= 0 && Index < Num);
+    return Data[Index];
+  }
 
   template<typename t_index>
   auto
   operator[](t_index Index) const
-    -> decltype(Data[Index]);
+    -> decltype(Data[Index])
+  {
+    BoundsCheck(Index >= 0 && Index < Num);
+    return Data[Index];
+  }
 };
 
 /// C++11 range API
 template<size_t N, typename t_element>
 t_element*
-begin(fixed_block<N, t_element>& Block);
+begin(fixed_block<N, t_element>& Block)
+{
+  return &Block.Data[0];
+}
 
 /// C++11 range API
 template<size_t N, typename t_element>
 t_element*
-end(fixed_block<N, t_element>& Block);
+end(fixed_block<N, t_element>& Block)
+{
+  return begin(Block) + Block.N;
+}
 
 /// C++11 range API
 template<size_t N, typename t_element>
 t_element*
-begin(fixed_block<N, t_element> const& Block);
+begin(fixed_block<N, t_element> const& Block)
+{
+  return &Block.Data[0];
+}
 
 /// C++11 range API
 template<size_t N, typename t_element>
 t_element*
-end(fixed_block<N, t_element> const& Block);
+end(fixed_block<N, t_element> const& Block)
+{
+  return begin(Block) + Block.N;
+}
 
 template<size_t N, typename t_element>
 slice<t_element>
-Slice(fixed_block<N, t_element>& Block);
+Slice(fixed_block<N, t_element>& Block)
+{
+  return { N, begin(Block) };
+}
 
 template<size_t N, typename t_element, typename t_start_index, typename t_end_index>
 slice<t_element>
-Slice(fixed_block<N, t_element>& Block, t_start_index InclusiveStartIndex, t_end_index ExclusiveEndIndex);
+Slice(fixed_block<N, t_element>& Block, t_start_index InclusiveStartIndex, t_end_index ExclusiveEndIndex)
+{
+  return Slice(Slice(Block), InclusiveStartIndex, ExclusiveEndIndex);
+}
 
 //]]~~
 
