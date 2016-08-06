7e7084ad96d7d1ae040d4011eb5498563b825b2e
Manuel Maier <mjmaier@gmx.de>
2016-08-07 01:35:24 +0200

    Make GenerateSingleHeader usable as module

--- Utilities/GenerateSingleHeader.py
+++ Utilities/GenerateSingleHeader.py
@@ -1,61 +1,53 @@
 from pathlib import Path
 import argparse
 
-Parser = argparse.ArgumentParser(description="Pack all Backbone sources into as few source files as possible to make distribution easier.")
-Parser.add_argument("outpath",
-                    type=Path,
-                    default=Path.cwd(),
-                    nargs="?",
-                    help="")
+def Generate(OutBasePath, BackboneRoot=None):
+  OutHeaderFilePath = OutBasePath / "Backbone.hpp"
+  OutTranslationUnitFilePath = OutBasePath / "Backbone.cpp"
 
-Args = Parser.parse_args()
+  BeginMarker = "~~[["
+  EndMarker   = "]]~~"
 
-OutBasePath = Args.outpath
-OutHeaderFilePath = OutBasePath / "Backbone.hpp"
-OutTranslationUnitFilePath = OutBasePath / "Backbone.cpp"
+  if not BackboneRoot:
+    BackboneRoot = Path(__file__).parent.parent.resolve()
+  CodePath = Path(BackboneRoot, "Code")
 
-BeginMarker = "~~[["
-EndMarker   = "]]~~"
+  NewLine = "\n"
 
-ThisDir = Path(__file__).parent.resolve()
-CodePath = Path(ThisDir.parent, "Code")
-
-NewLine = "\n"
-
-SeparateInlineFile = False
+  SeparateInlineFile = False
 
 
-def TransformLines(Lines):
-  Lower = 0
-  for Line in Lines:
-    Lower += 1
-    if BeginMarker in Line:
-      break
-  else:
+  def TransformLines(Lines):
     Lower = 0
+    for Line in Lines:
+      Lower += 1
+      if BeginMarker in Line:
+        break
+    else:
+      Lower = 0
 
-  Upper = len(Lines)
-  for Line in reversed(Lines):
-    Upper -= 1
-    if EndMarker in Line:
-      break
-  else:
     Upper = len(Lines)
+    for Line in reversed(Lines):
+      Upper -= 1
+      if EndMarker in Line:
+        break
+    else:
+      Upper = len(Lines)
 
-  return NewLine.join(L.rstrip("\r\n") for L in Lines[Lower : Upper])
+    return NewLine.join(L.rstrip("\r\n") for L in Lines[Lower : Upper])
 
 
-with OutHeaderFilePath.open("w", newline=NewLine) as OutFile:
-  def PrintLine(*args, **kwargs):
-    print(*args, **kwargs, file=OutFile)
+  with OutHeaderFilePath.open("w", newline=NewLine) as OutFile:
+    def PrintLine(*args, **kwargs):
+      print(*args, **kwargs, file=OutFile)
 
-  def PrintIncludeOrigin(FileName):
-    FileNameStr = FileName.as_posix()
-    PrintLine("// ============{}====".format("=" * len(FileNameStr)))
-    PrintLine("// === Source: {} ===".format(FileNameStr))
-    PrintLine("// ============{}====".format("=" * len(FileNameStr)))
+    def PrintIncludeOrigin(FileName):
+      FileNameStr = FileName.as_posix()
+      PrintLine("// ============{}====".format("=" * len(FileNameStr)))
+      PrintLine("// === Source: {} ===".format(FileNameStr))
+      PrintLine("// ============{}====".format("=" * len(FileNameStr)))
 
-  PrintLine("""
+    PrintLine("""
 // This file was generated using the tool Utilities/GenerateSingleHeader.py
 // from the Backbone project.
 
@@ -69,73 +61,73 @@ with OutHeaderFilePath.open("w", newline=NewLine) as OutFile:
 
 """)
 
-  FileName = Path("Backbone", "Common.hpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
-
-  FileName = Path("Backbone", "Memory.hpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
-
-  FileName = Path("Backbone", "Slice.hpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
-
-  FileName = Path("Backbone", "Angle.hpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
-
-  FileName = Path("Backbone", "Path.hpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
-
-  FileName = Path("Backbone", "FixedBlock.hpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
-
-  FileName = Path("Backbone", "StringConversion.hpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
-
-  if SeparateInlineFile:
-    assert False, "Not implemented."
-  else:
-    PrintLine("// ====================")
-    PrintLine("// === Inline Files ===")
-    PrintLine("// ====================")
-    PrintLine("")
-
-    FileName = Path("Backbone", "Path.inl")
+    FileName = Path("Backbone", "Common.hpp")
     with (CodePath / FileName).open("r") as File:
       TheLines = TransformLines(File.readlines())
       PrintIncludeOrigin(FileName)
       PrintLine(TheLines)
 
-with OutTranslationUnitFilePath.open("w", newline=NewLine) as OutFile:
-  def PrintLine(*args, **kwargs):
-    print(*args, **kwargs, file=OutFile)
+    FileName = Path("Backbone", "Memory.hpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
 
-  def PrintIncludeOrigin(FileName):
-    FileNameStr = FileName.as_posix()
-    PrintLine("// ============{}====".format("=" * len(FileNameStr)))
-    PrintLine("// === Source: {} ===".format(FileNameStr))
-    PrintLine("// ============{}====".format("=" * len(FileNameStr)))
+    FileName = Path("Backbone", "Slice.hpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
 
-  PrintLine("""
+    FileName = Path("Backbone", "Angle.hpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
+
+    FileName = Path("Backbone", "Path.hpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
+
+    FileName = Path("Backbone", "FixedBlock.hpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
+
+    FileName = Path("Backbone", "StringConversion.hpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
+
+    if SeparateInlineFile:
+      assert False, "Not implemented."
+    else:
+      PrintLine("// ====================")
+      PrintLine("// === Inline Files ===")
+      PrintLine("// ====================")
+      PrintLine("")
+
+      FileName = Path("Backbone", "Path.inl")
+      with (CodePath / FileName).open("r") as File:
+        TheLines = TransformLines(File.readlines())
+        PrintIncludeOrigin(FileName)
+        PrintLine(TheLines)
+
+  with OutTranslationUnitFilePath.open("w", newline=NewLine) as OutFile:
+    def PrintLine(*args, **kwargs):
+      print(*args, **kwargs, file=OutFile)
+
+    def PrintIncludeOrigin(FileName):
+      FileNameStr = FileName.as_posix()
+      PrintLine("// ============{}====".format("=" * len(FileNameStr)))
+      PrintLine("// === Source: {} ===".format(FileNameStr))
+      PrintLine("// ============{}====".format("=" * len(FileNameStr)))
+
+    PrintLine("""
 // This file was generated using the tool Utilities/GenerateSingleHeader.py
 // from the Backbone project.
 
@@ -146,32 +138,47 @@ with OutTranslationUnitFilePath.open("w", newline=NewLine) as OutFile:
 
 """)
 
-  FileName = Path("Backbone", "Common.cpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
+    FileName = Path("Backbone", "Common.cpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
 
-  FileName = Path("Backbone", "Slice.cpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
+    FileName = Path("Backbone", "Slice.cpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
 
-  FileName = Path("Backbone", "Memory.cpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
+    FileName = Path("Backbone", "Memory.cpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
 
-  FileName = Path("Backbone", "Angle.cpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
+    FileName = Path("Backbone", "Angle.cpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
 
-  FileName = Path("Backbone", "StringConversion.cpp")
-  with (CodePath / FileName).open("r") as File:
-    TheLines = TransformLines(File.readlines())
-    PrintIncludeOrigin(FileName)
-    PrintLine(TheLines)
+    FileName = Path("Backbone", "StringConversion.cpp")
+    with (CodePath / FileName).open("r") as File:
+      TheLines = TransformLines(File.readlines())
+      PrintIncludeOrigin(FileName)
+      PrintLine(TheLines)
+
+def Main():
+  Parser = argparse.ArgumentParser(description="Pack all Backbone sources into as few source files as possible to make distribution easier.")
+  Parser.add_argument("outpath",
+                      type=Path,
+                      default=Path.cwd(),
+                      nargs="?",
+                      help="")
+
+  Args = Parser.parse_args()
+  Generate(Args.outpath)
+
+if __name__ == '__main__':
+  Main()
+
